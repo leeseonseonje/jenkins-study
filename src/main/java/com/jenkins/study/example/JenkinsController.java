706ec413738060b28c0cf6a44cffce9a5b605798
seonje <leeseonje9323@gmail.com>
2023-01-29 02:04:03 +0900

    log 추가

--- src/main/java/com/jenkins/study/example/JenkinsController.java
+++ src/main/java/com/jenkins/study/example/JenkinsController.java
@@ -1,13 +1,16 @@
 package com.jenkins.study.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class JenkinsController {
 
     @GetMapping("/jenkins")
     public String helloJenkins() {
+        log.info("hello jenkins!");
         return "hello jenkins";
     }
 }
